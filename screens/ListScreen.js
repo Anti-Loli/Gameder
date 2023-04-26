296e5f1031ca7c94f3a9875d7f5870e8c97f5e0b
dmw46 <dmw46@pct.edu>
2023-04-25 20:42:50 -0400

    Branch correction

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,8 +1,39 @@
 import React , {useLayoutEffect} from "react";
 import Swiper from "react-native-deck-swiper"
-import { View, Text, Image, Button, StatusBar, TouchableOpacity, SafeAreaView } from "react-native";
+import { View, Text, Image, Button, StatusBar, TouchableOpacity, SafeAreaView, FlatList } from "react-native";
 import { Feather, MaterialCommunityIcons  } from '@expo/vector-icons';
 import styles from "../styles";
+
+const testData = [
+  {
+    id: '1',
+    name: "Item 1"
+  },
+  {
+    id: '2',
+    name: "Item 2"
+  },
+  {
+    id: '3',
+    name: "Item 3"
+  },
+  {
+    id: '4',
+    name: "Item 4"
+  },
+  {
+    id: '5',
+    name: "Item 5"
+  },
+];
+
+const renderItem = ({ item }) => (
+  <TouchableOpacity onPress={() => alert("Pressed ${item.name}")}>
+    <View>
+      <Text>{item.name}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 export default function ListScreen({ navigation }) {
   return (
@@ -24,6 +55,14 @@ export default function ListScreen({ navigation }) {
 
         <View style={styles.container}>
         <Text>List Screen</Text>
+        </View>
+        
+        <View style={{flex: 1}}>
+          <FlatList
+          data={testData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          />
         </View>
     </View>
   );
